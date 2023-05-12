b724d4f991e2a0928d4155a2a7d5c2c59ba41a4e
Aro Parada <haroldparada20@gmail.com>
2023-05-12 18:45:27 -0400

    aligned store title

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -4,7 +4,7 @@ import { productsArray } from "../productsStore";
 function Shop() {
   return (
     <>
-      <h1>Welcome to the Store</h1>
+      <h1 align="center" className="p-3">Welcome to the Store</h1>
       <Row xs={1} md={3} className="g-4">
         {productsArray.map((product, idx) => (
           <Col align="center" key={idx}>
