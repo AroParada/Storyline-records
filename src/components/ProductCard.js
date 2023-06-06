69bd72c802366cb60b2a1aa57f653a6039bad0ea
Aro Parada <haroldparada20@gmail.com>
2023-06-06 13:43:00 -0400

    added onclick addOneToCart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,21 @@
 import {Card, Button, Form, Row, Col } from 'react-bootstrap'
+import { CartContext } from '../CartContext';
+import { useContext } from 'react';
+
 
 function ProductCard(props) {  //props.product is the product we are selling
     const product = props.product;
-
+    // to access the cart object
+    const cart = useContext(CartContext)
+    const ProductQuantity = cart.getProductQuantity(product.id);
+    console.log(cart.items);
+    
     return (
         <Card>
             <Card.Body>
                 <Card.Title>{product.title}</Card.Title>
                 <Card.Text>${product.price}</Card.Text>
-                <Button variant='primary'>Add To Cart</Button>
+                <Button variant='primary' onClick={() => cart.addOneToCart(product.id)}>Add To Cart</Button>
             </Card.Body>
         </Card>
     )
