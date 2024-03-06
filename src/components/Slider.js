c075bb6bd92e16b7e7a4e669cbdb2d2d570fa34f
Aro Parada <haroldparada20@gmail.com>
2024-03-06 00:22:19 -0500

    added initalslide to swiperslide

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -13,6 +13,11 @@ import "../App.css";
 import { EffectCoverflow, Pagination } from "swiper";
 
 export default function App() {
+
+  // Calculate the index of the middle slide
+  const totalSlides = 8; // Change this to the total number of slides
+  const middleIndex = Math.floor(totalSlides / 2);
+
   return (
     <>
       <Swiper
@@ -30,32 +35,57 @@ export default function App() {
         }}
         pagination={false}
         modules={[EffectCoverflow, Pagination]}
+        initialSlide={middleIndex -1}
         className="mySwiper"
       >
         <SwiperSlide>
-          <img alt='albumCover' src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/white-stripes.jpg" />
+          <img
+            alt="albumCover"
+            src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/white-stripes.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img alt='albumCover' src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/107347-597a0e4334ba6-l.jpg" />
-        <h3> </h3>
+          <img
+            alt="albumCover"
+            src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/107347-597a0e4334ba6-l.jpg"
+          />
+          <h3> </h3>
         </SwiperSlide>
         <SwiperSlide>
-          <img alt='albumCover' src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/61Z-qRw050L._SL1076_.jpg" />
+          <img
+            alt="albumCover"
+            src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/61Z-qRw050L._SL1076_.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img alt='albumCover' src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/Like-Clockwork-Queens-of-the-Stone-Age-album-cover.jpg" />
+          <img
+            alt="albumCover"
+            src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/Like-Clockwork-Queens-of-the-Stone-Age-album-cover.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img alt='albumCover' src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/king-krule-ooz.jpg" />
+          <img
+            alt="albumCover"
+            src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/king-krule-ooz.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img alt='albumCover' src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/432374.jpg" />
+          <img
+            alt="albumCover"
+            src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/432374.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img alt='albumCover' src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/oh-sees-orc.jpg" />
+          <img
+            alt="albumCover"
+            src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/oh-sees-orc.jpg"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img alt='albumCover' src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/5ad0b35dc466b1bd1d6053c560a25c12.jpg" />
+          <img
+            alt="albumCover"
+            src="https://fictionalrecords004232c4232647c6a10668cb4de31c163357-staging.s3.amazonaws.com/5ad0b35dc466b1bd1d6053c560a25c12.jpg"
+          />
         </SwiperSlide>
       </Swiper>
     </>
