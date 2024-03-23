d2eeae2f2026d80eb38a3d34f3a3af051b2dad7b
Aro Parada <haroldparada20@gmail.com>
2024-03-23 00:18:11 -0400

    aligned img and text side by side in cart

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,29 +1,32 @@
-import Button from 'react-bootstrap/Button';
-import { CartContext } from '../CartContext'
-import { useContext } from 'react';
-import { getProductData } from '../productsStore';
-import { Image } from "@aws-amplify/ui-react";
-
-
+import Button from "react-bootstrap/Button";
+import { CartContext } from "../CartContext";
+import { useContext } from "react";
+import { getProductData } from "../productsStore";
+import { Image, Flex } from "@aws-amplify/ui-react";
+import { Col } from "react-bootstrap";
 
 function CartProduct(props) {
-    const cart = useContext(CartContext);
-    const id = props.id;
-    const quantity = props.quantity;
-    const productData = getProductData(id);
-    
-    return (
-      <>
-        <Image src={productData.image} alt="Amplify" width="8rem" />
+  const cart = useContext(CartContext);
+  const id = props.id;
+  const quantity = props.quantity;
+  const productData = getProductData(id);
+
+  return (
+    <Flex alignItems="flex-start">
+      <Image src={productData.image} alt="Amplify" width="8rem" />
+      <Flex direction="column" gap="xs">
         <h3>{productData.title}</h3>
         <p>{quantity} total</p>
         <p>${(quantity * productData.price).toFixed(2)}</p>
-        <Button size="sm" onClick={() => cart.deleteFromCart(id)}>
-          Remove
-        </Button>
+        <Col sm="6">
+          <Button size="sm" onClick={() => cart.deleteFromCart(id)}>
+            Remove
+          </Button>
+        </Col>
         <hr></hr>
-      </>
-    );
+      </Flex>
+    </Flex>
+  );
 }
 
-export default CartProduct
+export default CartProduct;
