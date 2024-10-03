fc562733873041b43103cf5092cfdfa43339dd0d
Harold parada <haroldparada20@gmail.com>
2024-10-03 01:13:58 -0400

    reset changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,19 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "fictionalrecords": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
   "auth": {
-    "fictionalrecords": {
+    "storylinerecords63b45f3a": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
