3722ba6d27f2a59ae84ba8af8c783adb1c07c374
Harold parada <haroldparada20@gmail.com>
2024-10-03 03:38:44 -0400

    fixed googleauth!

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,10 @@ export type AmplifyDependentResourcesAttributes = {
     "storylinerecords63b45f3a": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
