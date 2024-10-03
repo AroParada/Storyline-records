b80561b7eea1e0e85ef9e037ff742c8b8697e6fb
Harold parada <haroldparada20@gmail.com>
2024-10-03 03:57:21 -0400

    uncommented login card

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -1,26 +1,26 @@
-// import "@aws-amplify/ui-react/styles.css";
-// import {
-//   withAuthenticator,
-//   Button,
-//   Heading,
-//   View, 
-//   Card,
-// } from "@aws-amplify/ui-react";
-// import { Auth } from "aws-amplify";
-// import aws_exports from "../aws-exports";
-// Auth.configure(aws_exports);
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  View, 
+  Card,
+} from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
+import aws_exports from "../aws-exports";
+Auth.configure(aws_exports);
 
-// function LoginCard({ signOut }) {
-//   return (
-//     <View className="App">
-//       <Card>
-//         <Heading className='mb-5' level={1}>
-//           You are logged In!
-//         </Heading>
-//         <Button onClick={signOut}>Sign Out</Button>
-//       </Card>
-//     </View>
-//   );
-// }
+function LoginCard({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Heading className='mb-5' level={1}>
+          You are logged In!
+        </Heading>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Card>
+    </View>
+  );
+}
 
-// export default withAuthenticator(LoginCard);
+export default withAuthenticator(LoginCard);
