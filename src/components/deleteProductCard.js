5177a4a8b99a743104da05fe3fb7c391bd24c4ca
Harold parada <haroldparada20@gmail.com>
2024-12-18 16:52:56 -0500

    added pop up modal onclick

--- src/components/deleteProductCard.js
+++ src/components/deleteProductCard.js
@@ -1,8 +1,5 @@
 import { useContext, useState } from "react";
-import {
-  Card,
-  ListGroup,
-} from "react-bootstrap";
+import { Card, ListGroup, Button, Modal } from "react-bootstrap";
 import AWS from "aws-sdk";
 
 import { CartContext } from "../CartContext";
@@ -15,21 +12,52 @@ AWS.config.update({
 
 export default function DeleteProductCard() {
   const { products, loading } = useContext(CartContext);
+  const [modalShow, setModalShow] = useState(false);
+
+  const productClicked = (productId, productTitle) => {
+    console.log(`ID: ${productId}, title: ${productTitle}`);
+    setModalShow(true);
+  };
 
   return (
-    <Card className="adminCard">
-      <Card.Header>Remove a product</Card.Header>
-      <Card.Body>
-        <Card.Title>Remove product</Card.Title>
-        <ListGroup>
-          {products.map((product, idx) => (
-            <ListGroup.Item>
-              {product.title} - {product.artist}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      </Card.Body>
-      <Card.Footer className="text-muted"></Card.Footer>
-    </Card>
+    <>
+      <Card className="adminCard">
+        <Card.Header>Remove a product</Card.Header>
+        <Card.Body>
+          <Card.Title>Remove product</Card.Title>
+          <ListGroup>
+            {products.map((product) => (
+              <ListGroup.Item
+                key={product.id}
+                action
+                onClick={() => productClicked(product.id, product.title)}
+              >
+                {product.title} - {product.artist}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Card.Body>
+        <Card.Footer className="text-muted"></Card.Footer>
+      </Card>
+      <Modal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Delete Product
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Are you sure you want to delete product from shop</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button>Delete</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
