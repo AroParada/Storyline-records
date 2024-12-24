f8c3f1bcf5448ef4135ade04efb5145e53b545d6
Harold parada <haroldparada20@gmail.com>
2024-12-24 14:42:14 -0500

    added banner to home page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,28 @@
-import react from 'react';
+import { Button } from "react-bootstrap";
+import storePicture from "../assets/record-store-featured-2160574280.jpg";
 
-export default function Banner () {
-    return (
-        <div>
-            <Banner>hello</Banner>
-        </div>
-    )
+export default function Banner() {
+  return (
+    <div class="relative bg-neutral-800 p-8 text-start text-white w-full h-96 flex items-end">
+      <img
+        src={storePicture}
+        alt="Background"
+        className="opacity-25 absolute inset-0 w-full h-full object-cover z-0 filter grayscale"
+      />
+      <div className="relative z-10">
+        <h1 class="text-xxl font-bold align-bottom">Winter Sale</h1>
+        <Button variant="outline-light" size="lg" className="ms-2">
+          Shop Now
+        </Button>
+      </div>
+    </div>
+  );
 }
+
+// notes:
+// - The Button component is imported from react-bootstrap
+// - The image is imported from the assets folder
+// - The image is set as the background of the div
+// - The text is placed on top of the image using z-index
+// - The text is styled using Tailwind CSS classes
+// - The Button component is styled using Bootstrap classes
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,12 @@
 import Slider from "../components/Slider";
 import Featured from "../components/Featured";
+import Banner from "../components/Banner";
 import { Container } from "react-bootstrap";
 
 export function Home() {
   return (
     <>
-      <div class="bg-neutral-800 p-8 text-start text-white w-full h-35 ">
-        <h1 class="text-xl font-bold"></h1>
-      </div>
+      <Banner />
       <Container fluid>
         <Slider />
         <Featured />
