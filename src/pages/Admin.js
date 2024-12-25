e8ae5021930ae83735061e9904377de4502ca7c3
Harold parada <haroldparada20@gmail.com>
2024-12-25 00:09:56 -0500

    added banner to admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,15 +4,20 @@ import DeleteProductCard from "../components/deleteProductCard";
 
 export function Admin() {
   return (
-    <Container fluid>
-      <Row className="justify-content-center g-4">
-        <Col xs={12} sm={12} md={6}>
-          <AddProductCard />
-        </Col>
-        <Col xs={12} sm={12} md={6}>
-          <DeleteProductCard />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <div class="bg-neutral-800 p-8 text-start text-white w-full h-35 mb-5">
+        <h1 class="text-xl font-bold">Admin Page</h1>
+      </div>
+      <Container fluid>
+        <Row className="justify-content-center g-4">
+          <Col xs={12} sm={12} md={6}>
+            <AddProductCard />
+          </Col>
+          <Col xs={12} sm={12} md={6}>
+            <DeleteProductCard />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
