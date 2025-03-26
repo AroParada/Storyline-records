7760d411d4db4b21220f4f79abad7a10fae2affc
Harold parada <haroldparada20@gmail.com>
2025-03-25 22:31:52 -0400

    edit rows in shop

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -15,8 +15,8 @@ function Shop() {
       <div className="bg-neutral-800 p-8 text-start text-white w-full h-35 ">
         <h1 className="text-xl font-bold">Welcome to the Store</h1>
       </div>
-      <div className="mx-6 my-8 md:my-0 md:mx-0 md:m-12 lg:m-16 xl:px-24">
-        <Row xs={1} md={2} lg={2} xl={4} className="g-4">
+      <div className="mx-6 my-8 md:m-12 lg:m-16 xl:px-14">
+        <Row xs={1} md={2} lg={2} xl={3} className="g-4">
           {products.map((product, idx) => (
             <Col key={idx} align="center">
               <NewProductCard product={product} />
